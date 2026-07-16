02fbe221006cf5a976641fdccb4599c3c2db04aa
liudongyu <10185101162@stu.ecnu.edu.cn>
2026-07-16 10:57:15 +0800

    fix: 移除未使用的 event subscriber 注册

--- src/main/java/com/hexagram2021/initial_house/server/IHContent.java
+++ src/main/java/com/hexagram2021/initial_house/server/IHContent.java
@@ -2,16 +2,12 @@ package com.hexagram2021.initial_house.server;
 
 import com.hexagram2021.initial_house.server.register.*;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.EventBusSubscriber;
-
-import static com.hexagram2021.initial_house.InitialHouse.MODID;
 
 /**
  * 服务端内容注册入口，集中初始化结构生成相关的注册项喵~
  *
  * @author liudongyu
  */
-@EventBusSubscriber(modid = MODID)
 public final class IHContent {
 	/**
 	 * 执行模组构造阶段的注册初始化喵~
